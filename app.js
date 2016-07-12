dc6410eca6b367279df7d9f1443e87fc14e20e53
Brian0610 <brian49345@gmail.com>
2016-07-11 20:43:27 -0400

    update

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ $(function(){
 	$('nav a').on('click', function(e) {
 		e.preventDefault(); 
 		var url = this.href; 
-
 		$('#container').empty(); 
-		$('#container').load(url); 
-		
+		$('#container').load(url).hide().fadeIn(250);
 	}); 
 
 })
