bc67e06a3b7138c51e273f3ddcc21a73ac0d4f0e
Brian0610 <brian49345@gmail.com>
2016-07-11 20:40:07 -0400

    .load method

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ $(function(){
 	$('nav a').on('click', function(e) {
 		e.preventDefault(); 
 		var url = this.href; 
-		console.log(url); 
+
+		$('#container').empty(); 
+		$('#container').load(url); 
 		
 	}); 
 
